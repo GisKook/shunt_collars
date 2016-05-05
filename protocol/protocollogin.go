205b1869f8bd2074cb66270bc514eeef0c056025
zhangkai <zhangkai.gis@163.com>
2016-05-06 00:53:20 +0800

    modify imei

--- protocol/protocollogin.go
+++ protocol/protocollogin.go
@@ -1,8 +1,9 @@
 package protocol
 
 import (
-	"encoding/binary"
 	"fmt"
+	"log"
+	"strconv"
 )
 
 func CheckSum(buf []byte, length uint8) byte {
@@ -46,7 +47,25 @@ func NewLoginPakcet(imei uint32, protocolid uint8, sum uint8) *LoginPacket {
 
 func ParseLogin(buffer []byte) (*LoginPacket, *DasLoginPacket, uint32) {
 	protocolid := buffer[2]
-	imei := binary.BigEndian.Uint32(buffer[5:9])
+	termID0 := buffer[5]
+	termID1 := buffer[6] - 0x80
+	termID2 := buffer[7] - 0x80
+	termID3 := buffer[8]
+
+	strTermID0 := fmt.Sprintf("%02d", termID0)
+	strTermID1 := fmt.Sprintf("%02d", termID1)
+	strTermID2 := fmt.Sprintf("%02d", termID2)
+	strTermID3 := fmt.Sprintf("%02d", termID3)
+	log.Println(strTermID0)
+	log.Println(strTermID1)
+	log.Println(strTermID2)
+	log.Println(strTermID3)
+
+	strTermID := strTermID0 + strTermID1 + strTermID2 + strTermID3
+
+	tmp, _ := strconv.Atoi(strTermID)
+	imei := uint32(tmp)
+
 	sum := buffer[10]
 
 	return NewLoginPakcet(imei, protocolid, sum), NewDasLoginPacket(fmt.Sprint(imei)), imei
